d02d4b5a8620d0ed54bb4cef0d436c725d1b5183
vegan09 <hrnmdala7220@gmail.com>
2026-04-29 22:17:06 +0200

    added header

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zenith Academy</title>
+    <link rel="stylesheet" href="header.css">
+</head>
+<body>
+
+            <div class="header">
+                    <div class="one">
+                        <p class="app_title">ZENITH ACADEMY</p>
+                    </div>
+
+                    <div class="two">
+                        <a href="index.php" class="header_items">Home</a> &nbsp;
+                        <a href="index.php" class="header_items">Home</a> &nbsp;
+                        <a href="index.php" class="header_items">Home</a> &nbsp;
+                    </div>
+            </div>
+</body>
+</html>
